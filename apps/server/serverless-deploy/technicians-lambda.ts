d7471191ba03d6c19c08103b165167f86cb4efb5
unmin <unminaru@gmail.com>
2026-03-07 13:56:12 +0200

    initial frontend for technician calendar

--- apps/server/serverless-deploy/technicians-lambda.ts
+++ apps/server/serverless-deploy/technicians-lambda.ts
@@ -1,7 +1,7 @@
 import serverlessExpress from '@codegenie/serverless-express';
-import categoriesRoutes from '../src/modules/categories/categories.routes.js';
+import { techniciansRoutes } from '../src/modules/technicians/index.js';
 import app from '../src/shared-app.js';
 
-app.use('/api/categories/:categoryId/technicians', categoriesRoutes);
+app.use('/api/categories/:categoryId/technicians', techniciansRoutes);
 
 export const handler = serverlessExpress({ app });
